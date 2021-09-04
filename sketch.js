cba53852c6b393b474be33de9e5424403d2b84f1
khalk08 <76405797+khalk08@users.noreply.github.com>
2021-09-04 12:25:30 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var canvas;
+var gameState;
+var contestantCount;
+var database;
+var quiz, question, contestant;
+
+gameState = 0;
+
+function setup(){
+  canvas = createCanvas(850,400);
+  database = firebase.database;
+  quiz = new Quiz();
+  quiz.getState();
+  quiz.start();
+}
+
+
+function draw(){
+  background("pink");
+ 
+  if(playerCount === 4){
+    game.update(1);
+  }
+
+  if(gameState === 1){
+    play();
+    game.play();
+  }
+}
+
+
+
+
